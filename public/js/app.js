ca5f42469d1ff44ca41a2dd46ff1ac6947977d6a
Mortimer Soen <imorsoen@gmail.com>
2022-05-10 05:00:30 +0400

    Todo

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
 class Product extends React.Component {
+    constructor (props) {
+        super(props)
+        this.handleUpVote = this.handleUpVote.bind(this)
+    }
+
     render() {
         return (
             <div className='item'>
@@ -7,7 +12,7 @@ class Product extends React.Component {
                 </div>
                 <div className='middle aligned content'>
                     <div className='header'>
-                        <a>
+                        <a onClick={this.handleUpVote}>
                             <i className='large caret up icon' />
                         </a>
                         {this.props.votes}
@@ -29,6 +34,10 @@ class Product extends React.Component {
             </div>
         )
     }
+
+    handleUpVote() {
+        this.props.onVote(this.props.id)
+    }
 }
 
 class ProductList extends React.Component {
@@ -43,6 +52,7 @@ class ProductList extends React.Component {
                     votes={product.votes}
                     submitterAvatarUrl={product.submitterAvatarUrl}
                     productImageUrl={product.productImageUrl}
+                    onVote={this.handleProductUpVote}
                 />
         ))
 
@@ -51,6 +61,10 @@ class ProductList extends React.Component {
                 {productComponents}
             </div>
         )
+    }
+
+    handleProductUpVote(productId) {
+        console.log(productId)
     }
 }
 
